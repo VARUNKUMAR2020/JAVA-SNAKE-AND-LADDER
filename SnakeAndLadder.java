f19feab4930f00e28661ddb0bed4600e918298ff
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-16 15:13:50 +0530

    Add: Player rolls a die(using Random)

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,11 +1,19 @@
 package com.snakeandladder;
 
+import java.util.*;
+
 public class SnakeAndLadder {
-    static int playerone = 0;//declared player 1 position
+    static int playerOne = 0;// declared player 1 position
 
     public static void main(String[] args) {
         System.out.println("Game of Snake and Ladder");
         System.out.println("Welcome player A");
-        System.out.println("Player A in position : " + playerone);//Displaying player 1 position
+
+        // Random -to get number between 0 to 5:
+        Random random = new Random();
+        int roll = random.nextInt(5);
+        int rolled = roll + 1;
+        System.out.println("Player A rolled: " + rolled);//Display the number in the die
+        System.out.println("PLayer A is at position: " + playerOne + rolled);//Display the position in the board
     }
 }
