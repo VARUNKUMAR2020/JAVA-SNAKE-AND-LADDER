7897ad7eca4c538fe1080c6700fbfcc566b7c500
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-16 14:46:38 +0530

    Add: Single Player Position at 0

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,7 +1,11 @@
 package com.snakeandladder;
 
 public class SnakeAndLadder {
+    static int playerone = 0;//declared player 1 position
+
     public static void main(String[] args) {
         System.out.println("Game of Snake and Ladder");
+        System.out.println("Welcome player A");
+        System.out.println("Player A in position : " + playerone);//Displaying player 1 position
     }
 }
